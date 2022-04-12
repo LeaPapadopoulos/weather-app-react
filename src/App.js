6649e0f99fe146d3eb435040ec3377e4d38e8d42
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-04-12 10:43:59 +0200

    no referrer atribute added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
         <a
           href="https://github.com/LeaPapadopoulos/weather-app-react"
           target="_blank"
+          rel="noreferrer"
         ></a>
       </footer>
     </div>
