f6995fbb11c3ab1a51f00f7d3ee2889b4f062ea0
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-04-25 21:24:17 +0200

    main react app structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="container mt-3">
       <Weather />
       <footer>
         <a
@@ -15,6 +15,7 @@ export default function App() {
         >
           Open source code
         </a>
+        , by Lea
       </footer>
     </div>
   );
