7f59efe0639924b455134f2b3eb3ccdef0827f04
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-04-26 22:24:48 +0200

    fetch forecast weather

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import FormattedDate from "./FormattedDate";
+import WeatherForecast from "./WeatherForecast";
+
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -49,108 +51,77 @@ export default function Weather(props) {
 
   if (weatherData.ready) {
     return (
-          <div className="row g-0">
-            <div className="col-8 border align-bottom-parent">
-              <div className="p-3 align-bottom-text">
-                <h1>
-                  <span id="current-temp">{weatherData.temperature}</span>
-                  <span className="celsius-font">
-                    <span id="celsius-link"> °C</span>
-                  </span>
-                  <img src="" alt="" id="daily-icon" className="float-right" />
-                </h1>
-                <div className="float-right">
-                  <span>
-                    <span id="weather-description">
-                      {weatherData.description} in{" "}
-                    </span>
-                    <span id="selected-city">{weatherData.city}</span>
-                  </span>
-                </div>
-              </div>
-
-              <div className="border-top p-3 align-bottom-child">
-                <div id="current-date">
-                  <FormattedDate date={props.date} />
-                </div>
-              </div>
+      <div className="row g-0">
+        <div className="col-8 border align-bottom-parent">
+          <div className="p-3 align-bottom-text">
+            <h1>
+              <span id="current-temp">{weatherData.temperature}</span>
+              <span className="celsius-font">
+                <span id="celsius-link"> °C</span>
+              </span>
+              <img src="" alt="" id="daily-icon" className="float-right" />
+            </h1>
+            <div className="float-right">
+              <span>
+                <span id="weather-description">
+                  {weatherData.description} in{" "}
+                </span>
+                <span id="selected-city">{weatherData.city}</span>
+              </span>
             </div>
-            <div className="col-4 border-bottom border-top border-end">
-              <div className="border-bottom p-3">
-                <form
-                  onSubmit={handleSubmit}
-                  className="input-group float-top-right"
-                  id="search-form"
-                >
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder={weatherData.city}
-                    aria-label="city"
-                    aria-describedby="basic-addon2"
-                    id="search-text-input"
-                    autoComplete="off"
-                    onChange={handleCityChange}
-                  />
-                  <input
-                    type="submit"
-                    value="search"
-                    className="input-group-text"
-                    id="basic-addon2"
-                  />
-                </form>
-              </div>
-              <div className="border-bottom p-3">
-                <h4>Weather details</h4>
-                <div className="row">
-                  <div className="col-8">
-                    <div>Humidity</div>
-                    <div>Wind</div>
-                  </div>
-                  <div className="col-4">
-                    <div>
-                      <span id="humidity">{weatherData.humidity}</span> %
-                    </div>
-                    <div>
-                      <span id="wind">{weatherData.wind}</span> km/hr
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row p-3 g-2">
-                <h4>Next Days</h4>
+          </div>
 
-                <div className="col-3">
-                  <div className="shadow-sm bg-light pt-3 pb-3 text-center">
-                    <div id="day-1"></div>
-                    <img src="" alt="" id="day-icon-1" />
-                    <div id="day-temp-1"></div>
-                  </div>
+          <div className="border-top p-3 align-bottom-child">
+            <div id="current-date">
+              <FormattedDate date={props.date} />
+            </div>
+          </div>
+        </div>
+        <div className="col-4 border-bottom border-top border-end">
+          <div className="border-bottom p-3">
+            <form
+              onSubmit={handleSubmit}
+              className="input-group float-top-right"
+              id="search-form"
+            >
+              <input
+                type="text"
+                className="form-control"
+                placeholder={weatherData.city}
+                aria-label="city"
+                aria-describedby="basic-addon2"
+                id="search-text-input"
+                autoComplete="off"
+                onChange={handleCityChange}
+              />
+              <input
+                type="submit"
+                value="search"
+                className="input-group-text"
+                id="basic-addon2"
+              />
+            </form>
+          </div>
+          <div className="border-bottom p-3">
+            <h4>Weather details</h4>
+            <div className="row">
+              <div className="col-8">
+                <div>Humidity</div>
+                <div>Wind</div>
+              </div>
+              <div className="col-4">
+                <div>
+                  <span id="humidity">{weatherData.humidity}</span> %
                 </div>
-                <div className="col-3">
-                  <div className="shadow-sm bg-light pt-3 pb-3 text-center">
-                    <div id="day-2"></div>
-                    <img src="" alt="" id="day-icon-2" />
-                    <div id="day-temp-2"></div>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <div className="shadow-sm bg-light pt-3 pb-3 text-center">
-                    <div id="day-3"></div>
-                    <img src="" alt="" id="day-icon-3" />
-                    <div id="day-temp-3"></div>
-                  </div>
-                </div>
-                <div className="col-3">
-                  <div className="shadow-sm bg-light pt-3 pb-3 text-center">
-                    <div id="day-4"></div>
-                    <img src="" alt="" id="day-icon-4" />
-                    <div id="day-temp-4"></div>
-                  </div>
+                <div>
+                  <span id="wind">{weatherData.wind}</span> km/hr
                 </div>
               </div>
             </div>
           </div>
+          <WeatherForecast coordinates={weatherData.coordinates} />
+        </div>
+      </div>
     );
   } else {
     navigator.geolocation.getCurrentPosition(searchByLocation);
---
+++ src/WeatherForecast.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function forecastByLocation() {
+    let apiKey = "f5bebad10b3b825e71f74f7ce3f436a2";
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="row p-3 g-2">
+        <h4>Next Days</h4>
+
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 4) {
+            return (
+              <div className="col-3" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    );
+  } else {
+    forecastByLocation();
+    return null;
+  }
+}
